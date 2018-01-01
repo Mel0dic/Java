53aca781d232144f484d6a694bdff92ee3a3149e
Sectioned <Sectioned@users.noreply.github.com>
2018-01-01 13:26:12 +0000

    ok

--- Java Code/Matrix/matrix01.java
+++ Java Code/Matrix/matrix01.java
@@ -1,7 +1,27 @@
 import java.util.*;
+import java.io.IOException;
 
 public class matrix01{
 	public static void main(String[] args){
 		System.out.println("TestPrint");
+		//Linux Only
+		//System.out.print("\033[H\033[2J");
+		clearConsole();
+		System.out.flush();
+	}
+
+	public final static void clearConsole(){
+		final String osQuery = System.getProperty("os.name");
+
+		try{
+			if(osQuery.contains("Windows")){
+				Runtime.getRuntime().exec("cmd");
+			}
+			else{
+				Runtime.getRuntime().exec("clear");
+			}
+		}catch(){
+			//
+		}
 	}
 }
