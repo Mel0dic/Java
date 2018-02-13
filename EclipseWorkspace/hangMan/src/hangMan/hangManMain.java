e65b129bd8334e9703ef27e18f50cd242e284b8e
Sectioned <Sectioned@users.noreply.github.com>
2018-02-13 09:51:39 +0000

    ok

--- EclipseWorkspace/hangMan/src/hangMan/hangManMain.java
+++ EclipseWorkspace/hangMan/src/hangMan/hangManMain.java
@@ -16,7 +16,7 @@ public class hangManMain {
 	private static Random rand = new Random();
 	private static List<String> wordList = new ArrayList<>();
 	private static String wordToGuess;
-	private static int guesses = 0;
+	private static int guesses = 4;
 	private static Scanner reader = new Scanner(System.in);
 
 	public static void main(String[] args) {
@@ -48,25 +48,44 @@ public class hangManMain {
 	
 	public static void play(String word) {
 		System.out.println(word);
+		
 		int wordLength = word.length();
 		char[] wordArr = new char[wordLength];
+		List<Character> wrongLetters = new ArrayList<>();
+
+		
 		for(int i = 0; i < wordLength; i++) {
 			wordArr[i] = '_';
 		}
+		System.out.printf("Welcome to HangMan. Here's the word:%n");
 		System.out.println(wordArr);
 		
-		while(guesses < 6) {
+		int wordComplete = 0;
+		
+		while(guesses > 0 && wordLength > wordComplete) {
+			System.out.printf("Limbs left: %d%n", (guesses));
+			System.out.printf("Incorect guesses: ");
+			System.out.println(wrongLetters);
+			System.out.printf("Enter a letter to guess: ");
 			char s = reader.next().charAt(0);
 			if(word.indexOf(s) >= 0) {
 				for(int i = 0; i < wordLength; i++) {
 					if(s == word.charAt(i)) {
 						wordArr[i] = s;
+						wordComplete++;
 					}
 				}
 			}else {
-				guesses++;
+				System.out.printf("Sorry %s is not in the word.%n%n", s);
+				wrongLetters.add(s);
+				guesses--;
 			}
 			System.out.println(wordArr);
+		}
+		if(wordComplete == wordLength) {
+			System.out.println("You won");
+		}else {
+			System.out.println("Unlucky you lost.");
 		}
 	}
 }
