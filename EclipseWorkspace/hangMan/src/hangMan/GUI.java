77a42e49b40b922afed3b7cd580e6094aa5a10a1
Sectioned <Sectioned@users.noreply.github.com>
2018-02-14 12:14:37 +0000

    ok

--- EclipseWorkspace/hangMan/src/hangMan/GUI.java
+++ EclipseWorkspace/hangMan/src/hangMan/GUI.java
@@ -2,63 +2,55 @@ package hangMan;
 
 import java.awt.BasicStroke;
 import java.awt.Color;
-import java.awt.EventQueue;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.RenderingHints;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
-import java.awt.BorderLayout;
+import javax.swing.JTextField;
 
-public class GUI extends JFrame{
-
-	/**
-	 * Launch the application.
-	 */
+@SuppressWarnings("serial")
+public class GUI extends JPanel{
+	
+	JFrame jf;
+	private static int count = 0;
+	
+	public GUI() {
+		jf = new JFrame();
+		jf.setTitle("Test");
+		jf.setSize(600, 400);
+		jf.setVisible(true);
+		jf.setResizable(false);
+		jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		jf.getContentPane().add(this);
+	}
+	
 	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					GUI window = new GUI();
-					window.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
+		GUI gui = new GUI();
+		//count++;
+		//gui.repaint();
 	}
-
-	/**
-	 * Create the application.
-	 */
-	GUI() {
-		initialize();
-	}
-
-	/**
-	 * Initialize the contents of the frame.
-	 */
-	private void initialize() {
-		setBounds(100, 100, 450, 300);
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	
+	public void paintComponent(Graphics g) {
+		super.paintComponent(g);
+		Graphics2D g2 = (Graphics2D)g;
 		
-		JPanel p = new JPanel();
-		getContentPane().add(p, BorderLayout.CENTER);
-		
-//        Graphics g = p.getGraphics();
-//        Graphics2D g2 = (Graphics2D)g;
-        //g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-//        g2.setColor(Color.BLACK);
-//        g2.setStroke(new BasicStroke(10));
-//        g2.drawLine(0, 0, 20, 30);
-
-	}
-
-	private void paintComponent(Graphics g) {
-		super.paintComponents(g);
-		g.setColor(Color.BLACK);
-		g.drawRect(100, 10, 30, 40);
+		g2.setStroke(new BasicStroke(10));
+		g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+		g2.drawLine(420, 50, 420, 300);
+		g2.drawLine(420, 50, 545, 50);
+		g2.drawLine(400, 300, 570, 300);
+		g2.setStroke(new BasicStroke(7));
+		g2.drawLine(420, 240, 500, 300);
+		g2.fillOval(470, 100, 30, 30);
+		g2.drawLine(485, 110, 485, 175);
+		g2.drawLine(485, 175, 490, 185);
+		g2.drawLine(485, 175, 480, 185);
+		g2.drawLine(470, 140, 500, 140);
+		g2.setStroke(new BasicStroke(3));
+		g2.drawLine(485, 50, 485, 175);
 	}
 	
 }
