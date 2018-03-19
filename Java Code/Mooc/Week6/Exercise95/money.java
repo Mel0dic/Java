9d5a5eb2c3ed9a1d2bace3eea498f804ffc42fc8
Sectioned <Sectioned@users.noreply.github.com>
2018-03-19 20:19:32 +0000

    .

--- Java Code/Mooc/Week6/Exercise95/money.java
+++ Java Code/Mooc/Week6/Exercise95/money.java
@@ -5,22 +5,20 @@ public class money {
 
     public static void main(String[] args){
         money a = new money(10,0);
-        money b = new money(5,0);
+        money b = new money(3,50);
 
-        money c = a.plus(b);
+        money c = a.minus(b);
 
         System.out.println(a);  // 10.00e
-        System.out.println(b);  // 5.00e
-        System.out.println(c);  // 15.00e
+        System.out.println(b);  // 3.50e
+        System.out.println(c);  // 6.50e
 
-        a = a.plus(c);          // NOTE: new Money-object is created and reference to that
-                                //           is assigned to variable a.
-                                //       The Money object 10.00e that variable a used to hold
-                                //           is not referenced anymore
+        c = c.minus(a);         // NOTE: new Money-object is created and reference to that is assigned to variable c
+                        //       the Money object 6.50e that variable c used to hold, is not referenced anymore
 
-        System.out.println(a);  // 25.00e
-        System.out.println(b);  // 5.00e
-        System.out.println(c);  // 15.00e
+        System.out.println(a);  // 10.00e
+        System.out.println(b);  // 3.50e
+        System.out.println(c);  // 0.00e
     }
 
     public money(int euros, int cents) {
@@ -46,6 +44,38 @@ public class money {
         int euros = this.euros + money.euros();
         int cents = this.cents + money.cents();
         return new money(euros, cents);
+    }
+
+    public boolean less(money money){
+        if(this.euros > money.euros){
+            return false;
+        }else if(money.euros > this.euros){
+            return true;
+        }else if((money.euros == this.euros) && (this.cents > money.cents)){
+            return false;
+        }else{
+            return true;
+        }
+    }
+
+    public money minus(money decremented){
+        int newEuros = (this.euros - decremented.euros);//((this.euros - decremented.euros < 0) ? 0 : (this.euros - decremented.euros))
+        System.out.println(newEuros);
+        int newCents;
+        int tempCents = this.cents - decremented.cents;
+        if(tempCents < 0){
+            System.out.println("Hello");
+            System.out.println(newEuros);
+            if(newEuros < 0){
+                System.out.println("Hello");
+                return new money(0, 0);
+            }
+            newEuros--;
+            newCents = 100 + tempCents;
+        }else{
+            newCents = this.cents - decremented.cents;
+        }
+        return new money(newEuros, newCents);
     }
 
     public String toString() {
