afdf27f6d93884eb29707a2c33427a6e743725ea
Sectioned <Sectioned@users.noreply.github.com>
2018-03-19 20:58:21 +0000

    hmm

---
+++ Java Code/Mooc/Week6/Testing/charStrings.java
@@ -0,0 +1,10 @@
+public class charStrings{
+
+	public static void main(String[] args){
+		String characterString = "test";
+		characterString += "tail";
+
+		System.out.println( characterString );  // test
+	}
+
+}
