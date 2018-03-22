725c1a5118a19d3ab44b79de0f637be341dc086d
Sectioned <Sectioned@users.noreply.github.com>
2018-03-22 15:08:29 +0000

    ok

--- MainProject/movingDot.java
+++ MainProject/movingDot.java
@@ -83,25 +83,3 @@ public class movingDot{
 	}
 
 }
-
-class Control implements KeyListener{
-
-	int puckDirection = 0;
-
-	@Override
-	public void keyPressed(KeyEvent e) {
-		// TODO Auto-generated method stub
-		if(e.getKeyCode()==e.VK_UP){
-			puckDirection = -1;
-		}else if(e.getKeyCode()==e.VK_DOWN){
-			puckDirection = 1;
-		}
-
-	}
-
-	@Override
-	public void keyReleased(KeyEvent e) {
-		puckDirection = 0;
-	}
-	
-}
