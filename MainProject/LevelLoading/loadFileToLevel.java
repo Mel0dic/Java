67e56a26cfb4ad49e36ee24f0a34c06dc939e530
Sectioned <Sectioned@users.noreply.github.com>
2018-03-26 20:50:12 +0100

    ok

---
+++ MainProject/LevelLoading/loadFileToLevel.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class loadFileToLevel{
+
+	public static void main(String[] args){
+		
+	}
+
+	public void openFile() throws IOException{
+		//Get file location and create instance of the file "converts given pathname string into an abstract pathname"
+		File file = new File("level.txt");
+		//Create BufferedReader with FileReader
+		BufferedReader br = new BufferedReader(new FileReader(file));
+		//Initialize empty string
+		String line = null;
+
+		//While the br.readLine() does not return null
+		while( (line = br.readLine())!= null ){
+			System.out.println(line);
+		}
+	}
+
+}
