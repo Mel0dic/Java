90beaabc05ed713d87553b71e1fbcaef3457639a
Sectioned <Sectioned@users.noreply.github.com>
2018-07-09 19:18:53 +0100

    ok

--- Java Code/DailyProblems/100-200/121/PalindromeCreation.java
+++ Java Code/DailyProblems/100-200/121/PalindromeCreation.java
@@ -11,7 +11,18 @@ public class PalindromeCreation{
 	 * For example, given 'waterrfetawx' and a k of 2, you could delete f and x to get 'waterretaw'.
 	**/
 	public static String createPalindrome(String givenString, int numberOfLetterDeletable){
+		String[] stringLetters = givenString.split("");
+		for(int i = 0; i < givenString.length(); i++){
+			System.out.println(stringLetters[i]);
+		}
 
+
+		StringBuilder strBuilder = new StringBuilder();
+		for (int i = 0; i < stringLetters.length; i++) {
+		   strBuilder.append(stringLetters[i]);
+		}
+		String newString = strBuilder.toString();
+		return stringLetters.toString();
 	}
 
 }
