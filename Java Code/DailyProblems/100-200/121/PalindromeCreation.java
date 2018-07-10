b88cb07c0b6b07f854e15c4dee53acb9134fe381
Sectioned <Sectioned@users.noreply.github.com>
2018-07-10 21:45:47 +0100

    ok

--- Java Code/DailyProblems/100-200/121/PalindromeCreation.java
+++ Java Code/DailyProblems/100-200/121/PalindromeCreation.java
@@ -5,9 +5,9 @@ import java.util.List;
 public class PalindromeCreation{
 
 	public static void main(String[] args){
-		System.out.println(isPalindrome("HellolleH"));
-		System.out.println(isPalindrome("OlevpvolO"));
-		System.out.println(isPalindrome("Orangegnaro"));
+		// System.out.println(isPalindrome("HellolleH"));
+		// System.out.println(isPalindrome("OlevpvolO"));
+		// System.out.println(isPalindrome("Orangegnaro"));
 
 		System.out.println(createPalindrome("waterrfetawx", 2));
 	}
@@ -19,22 +19,25 @@ public class PalindromeCreation{
 	 * For example, given 'waterrfetawx' and a k of 2, you could delete f and x to get 'waterretaw'.
 	**/
 	public static Boolean createPalindrome(String givenString, int numberOfLettersDeletable){
-		String[] stringLetters = givenString.split("");
+		char[] stringLetters = givenString.toCharArray();
 
-		if(isPalindrome(givenString)){return true;}
+		if(isPalindrome(givenString) || givenString.length() <= 1){return true;}
 
-		int minHalfWayPoint = (int) (stringLetters.length / 2) - (numberOfLettersDeletable / 2);
-		int maxHalfwayPoint =  ((int) (stringLetters.length / 2)) - 1;
+		while(stringLetters[0] == stringLetters[stringLetters.length-1]){
+			stringLetters = Arrays.copyOfRange(stringLetters, 1, stringLetters.length-2);
+		}
 
-		char[][] lists = flipList(givenString);
+		if(numberOfLettersDeletable == 0){
+			return false;
+		}
 
-		return true;
+		return createPalindrome(String.valueOf(Arrays.copyOfRange(stringLetters, 0 , stringLetters.length-2)), numberOfLettersDeletable - 1) || createPalindrome(String.valueOf(Arrays.copyOfRange(stringLetters, 1, stringLetters.length-1)), numberOfLettersDeletable - 1);
 	}
 
 	public static boolean isPalindrome(String palandrome){
 		char[] stringArray = palandrome.toCharArray();
 		int stringHalfSize = (int)(stringArray.length / 2);
-		char[][] tempArray = flipList(stringArray, stringHalfSize);
+		char[][] tempArray = copyList(stringArray, stringHalfSize);
 		for(int i = 0; i < stringHalfSize; i++){
 			if(tempArray[0][i] != tempArray[1][i]){
 				return false;
@@ -43,7 +46,7 @@ public class PalindromeCreation{
 		return true;
 	}
 
-	public static char[][] flipList(char[] listToFlip, int stringHalfSize){
+	public static char[][] copyList(char[] listToFlip, int stringHalfSize){
 		char[][] stringBreakDown = new char[2][stringHalfSize];
 		if(listToFlip.length % 2 == 0){
 			stringBreakDown[0] = Arrays.copyOfRange(listToFlip, 0, stringHalfSize);
@@ -52,12 +55,16 @@ public class PalindromeCreation{
 			stringBreakDown[0] = Arrays.copyOfRange(listToFlip, 0, stringHalfSize);
 			stringBreakDown[1] = Arrays.copyOfRange(listToFlip, (stringHalfSize + 1), listToFlip.length);
 		}
-		char[] tempArray = new char[stringHalfSize];
-		for(int i = stringBreakDown[0].length-1, y = 0; i >= 0; i--, y++){
-			tempArray[y] = stringBreakDown[1][i];
-		}
-		stringBreakDown[1] = tempArray;
+		stringBreakDown[1] = flipList(stringBreakDown[1]);
 		return stringBreakDown;
+	}
+
+	public static char[] flipList(char[] listToFlip){
+		char[] tempArray = new char[listToFlip.length];
+		for(int i = listToFlip.length-1, y = 0; i >= 0; i--, y++){
+			tempArray[y] = listToFlip[i];
+		}
+		return tempArray;
 	}
 
 }
