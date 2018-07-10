9f0f97f291fc37ab879b55a1437c6b897930ebd8
Sectioned <Sectioned@users.noreply.github.com>
2018-07-10 14:50:21 +0100

    ok

--- Java Code/DailyProblems/100-200/120/Singleton.java
+++ Java Code/DailyProblems/100-200/120/Singleton.java
@@ -1,3 +1,9 @@
+/**
+ * Implement the singleton pattern with a twist. First, instead of storing one instance, 
+ * store two instances. And in every even call of getInstance(), return the first instance 
+ * and in every odd call of getInstance(), return the second instance.
+ */
+
 public class Singleton{
 
 	private static Singleton singletonInstanceOne = null;
