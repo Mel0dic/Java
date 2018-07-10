1b18f5f6d7494f397ebc01fdaa77be32f1b1b8e9
Sectioned <Sectioned@users.noreply.github.com>
2018-07-10 15:37:05 +0100

    ok

--- Java Code/DailyProblems/100-200/121/PalindromeCreation.java
+++ Java Code/DailyProblems/100-200/121/PalindromeCreation.java
@@ -9,7 +9,7 @@ public class PalindromeCreation{
 		System.out.println(isPalindrome("OlevpvolO"));
 		System.out.println(isPalindrome("Orangegnaro"));
 
-		System.out.println(createPalindrome("TestingnitsgeT", 4));
+		System.out.println(createPalindrome("waterrfetawx", 2));
 	}
 
 
@@ -26,40 +26,38 @@ public class PalindromeCreation{
 		int minHalfWayPoint = (int) (stringLetters.length / 2) - (numberOfLettersDeletable / 2);
 		int maxHalfwayPoint =  ((int) (stringLetters.length / 2)) - 1;
 
-		for(int i = minHalfWayPoint; i <= maxHalfwayPoint; i++){
-			System.out.println(i);
-		}
+		char[][] lists = flipList(givenString);
 
-		StringBuilder strBuilder = new StringBuilder();
-		for (int i = 0; i < stringLetters.length; i++) {
-		   strBuilder.append(stringLetters[i]);
-		}
-		String newString = strBuilder.toString();
 		return true;
 	}
 
 	public static boolean isPalindrome(String palandrome){
 		char[] stringArray = palandrome.toCharArray();
 		int stringHalfSize = (int)(stringArray.length / 2);
-		char[] firstHalf = new char[stringHalfSize];
-		char[] secondHalf = new char[stringHalfSize];
-		if(stringArray.length % 2 == 0){
-			firstHalf = Arrays.copyOfRange(stringArray, 0, stringHalfSize);
-			secondHalf = Arrays.copyOfRange(stringArray, (stringHalfSize), stringArray.length);
-		}else{
-			firstHalf = Arrays.copyOfRange(stringArray, 0, stringHalfSize);
-			secondHalf = Arrays.copyOfRange(stringArray, (stringHalfSize + 1), stringArray.length);
-		}
-		List<Character> tempList = new ArrayList<Character>();
-		for(int i = firstHalf.length-1; i >= 0; i--){
-			tempList.add(firstHalf[i]);
-		}
+		char[][] tempArray = flipList(stringArray, stringHalfSize);
 		for(int i = 0; i < stringHalfSize; i++){
-			if(tempList.get(i) != secondHalf[i]){
+			if(tempArray[0][i] != tempArray[1][i]){
 				return false;
 			}
 		}
 		return true;
+	}
+
+	public static char[][] flipList(char[] listToFlip, int stringHalfSize){
+		char[][] stringBreakDown = new char[2][stringHalfSize];
+		if(listToFlip.length % 2 == 0){
+			stringBreakDown[0] = Arrays.copyOfRange(listToFlip, 0, stringHalfSize);
+			stringBreakDown[1] = Arrays.copyOfRange(listToFlip, (stringHalfSize), listToFlip.length);
+		}else{
+			stringBreakDown[0] = Arrays.copyOfRange(listToFlip, 0, stringHalfSize);
+			stringBreakDown[1] = Arrays.copyOfRange(listToFlip, (stringHalfSize + 1), listToFlip.length);
+		}
+		char[] tempArray = new char[stringHalfSize];
+		for(int i = stringBreakDown[0].length-1, y = 0; i >= 0; i--, y++){
+			tempArray[y] = stringBreakDown[1][i];
+		}
+		stringBreakDown[1] = tempArray;
+		return stringBreakDown;
 	}
 
 }
