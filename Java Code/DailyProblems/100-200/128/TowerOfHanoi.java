1b7353effef5919a99e1a656b92e0dd6cf8c7d06
Sectioned <Sectioned@users.noreply.github.com>
2018-07-17 17:58:41 +0100

    ok

--- Java Code/DailyProblems/100-200/128/TowerOfHanoi.java
+++ Java Code/DailyProblems/100-200/128/TowerOfHanoi.java
@@ -3,13 +3,13 @@ import java.util.ArrayList;
 
 public class TowerOfHanoi{
 
-	public static String stringFormat = "Move %i to %i";
+	public static String stringFormat = "Move %d to %d";
 	private List<Integer> rodOne;
 	private List<Integer> rodTwo;
 	private List<Integer> rodThree;
 
 	public static void main(String[] args){
-		new TowerOfHanoi(3).printMoves();
+		new TowerOfHanoi(4);
 	}
 
 	public TowerOfHanoi(int numberOfDisks){
@@ -19,13 +19,23 @@ public class TowerOfHanoi{
 		for(int i = numberOfDisks; i > 0; i--){
 			rodOne.add(i);
 		}
+		printMoves(numberOfDisks);
 	}
 
-	public void printMoves(){
-		for(int i : rodOne){
-			System.out.println(i);
+	public void printMoves(int numberOfDisks){
+		if(numberOfDisks >= 1){
+			printMoves(numberOfDisks-1, 1, 3, 2);
+			System.out.println(String.format(stringFormat, 1, 3));
+			printMoves(numberOfDisks-1, 2, 1, 3);
 		}
-		System.out.println(rodThree.size());
+	}
+
+	public void printMoves(int numberOfDisks, int a, int b, int c){
+		if(numberOfDisks >= 1){
+			printMoves(numberOfDisks-1, a, c, b);
+			System.out.println(String.format(stringFormat, a, c));
+			printMoves(numberOfDisks-1, b, a, c);
+		}
 	}
 
 }
